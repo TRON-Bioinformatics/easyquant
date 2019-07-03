e9e694226219883450e2ee48bae48bf4f0fbc684
Patrick Sorn <patrick.sorn@tron-mainz.de>
2019-07-03 15:52:48 +0200

    Initial commit

---
+++ get_neo_orfs.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+import pwd,grp,stat
+import time
+import csv
+import math
+from argparse import ArgumentParser
+import misc.io_methods as IOMethods
+from misc.config import Config
+from misc.samples import Samples
+import misc.queue as Queueing
+
+
+class CustomTranscriptome(object):
+    def __init__(self,cfg,input,fusions_table,working_dir,partitions):
+        csv.field_size_limit(999999999)
+        self.cfg = cfg
+        self.input = []
+        for i in input:
+            self.input.append(i.rstrip("/"))
+        self.fusions_table = fusions_table
+        self.working_dir = working_dir.rstrip("/")
+        IOMethods.create_folder(self.working_dir)
+        self.samples = Samples(os.path.join(self.working_dir,"samples.csv"))
+        self.fasta = os.path.join(self.working_dir,"context.fa")
+        self.bed = os.path.join(self.working_dir,"context.bed")
+        self.partitions = partitions
+        self.csv_to_fasta()
+        self.generate_bed()
+        self.build_index()
+
+
+    def get_fastq_files(self,paths):
+        '''This function returns a list of fastq files for the given list of paths.'''
+        fastqs = []
+        for path in paths:
+            if os.path.isdir(path):
+                files = os.listdir(path)
+                for file in files:
+                    file_path = os.path.join(path,file)
+                    if os.path.isfile(file_path) and file.endswith((".fastq.gz",".fq.gz",".fq",".fastq")):
+                        fastqs.append(file_path)
+                    elif os.path.isdir(file_path):
+                        fastqs_tmp = self.get_fastq_files([file_path])
+                        fastqs.extend(fastqs_tmp)
+            elif os.path.isfile(path) and path.endswith((".fastq.gz",".fq.gz",".fq",".fastq")):
+                fastqs.append(path)
+        return fastqs
+
+    def calc_genome_size(self):
+        genome_size = 0
+        with open(self.fasta) as inf:
+            for line in inf:
+                if not line.rstrip().startswith(">"):
+                    genome_size += len(line.rstrip())
+        print "Custom Genome Size: " + str(genome_size) + "bp"
+        return genome_size
+
+### FGID;Fusion_Gene;Breakpoint1;Breakpoint2;FTID;context_sequence_id;type;exon_boundary1;exon_boundary2;exon_boundary;bp1_frame;bp2_frame;frame;wt1_expression;wt2_expression;context_sequence;context_sequence_bp;wt1_context_sequence;wt1_context_sequence_bp;wt2_context_sequence;wt2_context_sequence_bp;neo_peptide_sequence;neo_peptide_sequence_bp;transcript_sequence ###
+
+    def csv_to_fasta(self):
+        outf_context = open(self.fasta,"w")
+        with open(self.fusions_table) as csvfile:
+            inf = csv.reader(csvfile, delimiter=';')
+            header = inf.next()
+
+            col = {}
+            for i,colname in enumerate(header):
+                col[colname] = i
+            for c,row in enumerate(inf):
+                ftid = row[col["FTID"]]
+                fgid = row[col["FGID"]]
+                context_id = row[col["context_sequence_id"]]
+                context_seq = row[col["context_sequence"]].upper()
+
+                outf_context.write(">" + context_id + "\n")
+                for i in range(0,len(context_seq),60):
+                    outf_context.write(context_seq[i:i+60]+"\n")
+        outf_context.close()
+
+    def generate_bed(self):
+        outf_context = open(self.bed,"w")
+        outf_context_vis = open(self.bed+".visualisation.bed","w")
+        with open(self.fusions_table) as csvfile:
+            inf = csv.reader(csvfile, delimiter=';')
+            header = inf.next()
+            col = {}
+            for i,colname in enumerate(header):
+                col[colname] = i
+
+            for c,row in enumerate(inf):
+                ftid = row[col["FTID"]]
+                fgid = row[col["FGID"]]
+                context_id = row[col["context_sequence_id"]]
+                context_sequence = row[col["context_sequence"]]
+
+                genes = fgid.split("_", 1)
+                gene1 = genes[0]
+                gene2 = genes[1]
+                
+                rel_bp = row[col["context_sequence_bp"]]
+
+                location = 0
+                if rel_bp != "NA":
+                    location = int(rel_bp)
+
+                strand1 = "+"
+                strand2 = "+"
+
+                start = 0
+                end1 = location
+                end2 = len(context_sequence)
+
+                outf_context.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (context_id,start,end1,gene1,"",strand1,start,end1,"0",1,end1,start))
+                outf_context.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (context_id,end1,end2,gene2,"",strand2,end1,end2,"0",1,end2-end1,end1))
+                
+                outf_context_vis.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (context_id,start,(end1-5),gene1,"",strand1,start,(end1-5),"0",1,(end1-5),start))
+                outf_context_vis.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (context_id,(end1+5),end2,gene2,"",strand2,(end1+5),end2,"0",1,end2-(end1+5),(end1+5)))
+
+        outf_context.close()
+        outf_context_vis.close()
+
+    def build_index(self):
+        star_idx_path = os.path.join(self.working_dir,"STAR_idx")
+
+        for folder in [star_idx_path]:
+            IOMethods.create_folder(folder)
+
+        shell_script = os.path.join(self.working_dir,"generate_index.sh")
+        out_shell = open(shell_script,"w")
+        sa_index_nbases = int(math.log(self.calc_genome_size()) / 2 - 1)
+        cmd_star = "%s --runMode genomeGenerate --runThreadN 12 --genomeSAindexNbases %s --genomeDir %s --genomeFastaFiles %s" % (self.cfg.get('commands','star_cmd'),sa_index_nbases,star_idx_path,self.fasta)
+
+        out_shell.write("#!/bin/sh\n\n")
+        out_shell.write("working_dir="+self.working_dir+"\n")
+        out_shell.write("echo \"Generating STAR index\"\n")
+        out_shell.write(cmd_star+"\n")
+        out_shell.write("echo \"Processing done!\"\n")
+        out_shell.close()
+
+        if not os.path.exists(os.path.join(star_idx_path,"Genome")):
+            self.submit_job_nonqueue(cmd_star,star_idx_path)
+
+    def run(self):
+        '''This function starts processing of the samples.'''
+        fastqs = self.get_fastq_files(self.input)
+        file_array = sorted(fastqs)
+#        print file_array
+        left = []
+        right = []
+        for i in range(len(file_array)):
+            file_array_split = file_array[i].rsplit("_",2)
+            skewer_split = ""
+            try:
+                skewer_split = file_array[i].rsplit("-",1)[1].rsplit(".",2)[0]
+            except:
+                skewer_split = ""
+            if "R1" in file_array_split[1] or skewer_split == "pair1":
+                left.append(file_array[i])
+            elif "R2" in file_array_split[1] or skewer_split == "pair2":
+                right.append(file_array[i])
+        for i,ele in enumerate(left):
+
+            if len(right) != 0:
+                print "Sample 1: " + left[i]
+                print "Sample 2: " + right[i]
+                self.execute_pipeline(left[i],right[i])
+
+    def execute_pipeline(self,file_1,file_2):
+
+        star_genome_path = os.path.join(self.working_dir,"STAR_idx")
+
+        star_path = os.path.join(self.working_dir,"star")
+
+        for folder in [star_path]:
+            IOMethods.create_folder(folder)
+        shell_script = os.path.join(self.working_dir,"requant.sh")
+        out_shell = open(shell_script,"w")
+
+        bam_file = os.path.join(star_path,"Aligned.sortedByCoord.out.bam")
+
+        cmd_star = "%s --limitOutSAMoneReadBytes 1000000 --genomeDir %s --readFilesCommand 'gzip -d -c -f' --readFilesIn %s %s --outSAMmode Full --outFilterMultimapNmax -1 --outSAMattributes Standard --outSAMunmapped None --outFilterMismatchNoverLmax 0.02 --outSAMtype BAM SortedByCoordinate --runThreadN 12 && %s index %s" % (self.cfg.get('commands','star_cmd'), star_genome_path, file_1, file_2, self.cfg.get('commands', 'samtools_cmd'), bam_file)
+
+        cmd_class = "%s -i %s -b %s -o %s" % (self.cfg.get('commands', 'classification_cmd'), bam_file, os.path.join(self.working_dir, self.bed), os.path.join(self.working_dir, "Classification.csv"))
+
+        out_shell.write("#!/bin/sh\n\n")
+        out_shell.write("fq1="+file_1+"\n")
+        if file_2:
+            out_shell.write("fq2="+file_2+"\n")
+        out_shell.write("working_dir="+self.working_dir+"\n")
+        out_shell.write("echo \"Starting pipeline...\"\n")
+        if file_2:
+            out_shell.write("echo \"Starting STAR\"\n")
+            out_shell.write(cmd_star+"\n")
+            out_shell.write("echo \"Starting Classification\"\n")
+            out_shell.write(cmd_class+"\n")
+        out_shell.write("echo \"Processing done!\"\n")
+        out_shell.close()
+
+        if not os.path.exists(os.path.join(star_path, "Log.final.out")):
+            self.submit_job_nonqueue(cmd_star, star_path)
+
+        if not os.path.exists(os.path.join(self.working_dir, "Classification.csv")):
+            self.submit_job_nonqueue(cmd_class, self.working_dir)
+
+        print "Processing complete for " + self.working_dir
+
+    def submit_job_nonqueue(self,cmd,output_results_folder):
+        Queueing._submit_nonqueue(cmd,output_results_folder)
+
+    def submit_job(self,module,sample_id,cmd,cores,mem_usage,output_results_folder,dependencies):
+        '''This function submits a job with the corresponding resources to slurm.'''
+        q = Queue()
+        job_name = "-".join([module,sample_id])
+        already_running = self.get_jobs_with_name(job_name)
+        if len(already_running) == 0:
+
+            q.submit_slurm(job_name,cmd,cores,mem_usage,output_results_folder,dependencies,self.partitions)
+            time.sleep(3)
+        else:
+            print job_name + " already running!"
+
+        return job_name
+       
+
+def main():
+    parser = ArgumentParser(description='Processing of demultiplexed FASTQs')
+
+    parser.add_argument('-i', '--input', dest='input', nargs='+', help='Specify the fastq folder(s) or fastq file(s) to process.',required=True)
+    parser.add_argument('-f', '--fusions', dest='fusions', help='Specify the reference fusions table to base index on.',required=True)
+#    parser.add_argument('-m','--min-read-length-perc',dest='min_read_length_perc',type=float,help='Specify minimum read length percentage',default=0.75)
+#    parser.add_argument('--no-qc', dest='no_qc', action='store_true', help='Specify if QC shall be skipped on samples')
+    parser.add_argument('-o', '--output-folder', dest='output_folder', help='Specify the folder to save the results into.',required=True)
+
+    parser.add_argument('-p','--partitions',dest='partitions',help='Comma-separated list of partitions to use for queueing.',default='Compute')
+    args = parser.parse_args()
+
+    cfg = Config(os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.ini'))
+
+    script_call = "python " + os.path.realpath(__file__) + " " + " ".join(sys.argv[1:])
+    
+    ct = CustomTranscriptome(cfg,args.input,args.fusions,args.output_folder,args.partitions)
+    ct.run()
+
+    outf = open(os.path.join(args.output_folder,"custom_transcriptome.sh"),"w")
+    outf.write("#!/bin/sh\n\n")
+    outf.write(script_call)
+    outf.close()
+
+if __name__ == '__main__':
+    main()
---
+++ requantify.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+"""
+Junction/Spanning read pair counting
+
+@author: BNT (URLA)
+@version: 20190118
+"""
+
+from __future__ import print_function, division
+import os.path
+from argparse import ArgumentParser
+#from urla_logger_latest import Logger
+import pysam # pysam is not available for windows (where I run pylint) => pylint: disable=E0401
+
+
+# pylint: disable=line-too-long
+#         yes they are partially, but I do not consider this to be relevant here
+class Requantification(object):
+    """Select alignments belonging to putative fusions from an s/bam file"""
+    def __init__(self, bam, bed, output, bp_distance_threshold):
+        """Parameter initialization"""
+        self.in_bam = pysam.AlignmentFile(bam, "rb")
+        self.bed = bed
+        self.output = output
+        self.bp_distance_threshold = int(bp_distance_threshold)
+        self.fusion_seq_dict = self.get_fusions()
+        #self.logger = Logger("{}.fusionReadFilterLog".format(output))
+        self.input_read_count = 0
+
+    def get_fusions(self):
+        fusion_seq_dict = {}
+        with open(self.bed) as bed:
+            for line in bed:
+                elements = line.rstrip().split("\t")
+                fgid = elements[0]
+                bp = elements[1]
+
+                fusion_seq_dict[fgid] = [int(bp), 0, 0, 0, 0, 0]
+
+        return fusion_seq_dict
+
+    def quantify_read_groups(self, read_buffer, reference_base):
+        """Count junction/spanning pairs on ft and wt1/2"""
+        anchor = 0
+        # the read_buffer contains all reads mapping to the fusion transcript and the wildtyp backgrounds
+        # the read_group is list of reads (incl multimappers) belonging to a single read pair
+        # reads from this read group can map independently to ft, wt1 and wt2
+        for read_group in read_buffer:
+            junction = 0 # 0 = no junction overlapping read, 1+ = at least one of the paired reads is a junction read
+            spanning = [0, 0] # left: paired read cnt fully left of the fusion bp, right: paired read cnt fulls right of the fusion bp
+
+            # each processed read belongs to one of the three defined read_groups
+            for read in read_buffer[read_group]:
+                junction, spanning, anchor = self.count_junc_span(read, self.fusion_seq_dict[reference_base][0], junction, spanning, anchor)
+
+            #print("stored before: {}".format(self.fusion_seq_dict[reference_base]))
+            #print("jft: {}, sft: {}, jwt: {}, swt: {}".format(is_junction_ft, is_spanning_ft, is_junction_wt, is_spanning_wt))
+            # update junction and spanning counts
+            self.update_counts(reference_base, junction, spanning, 1)
+        # set max anchor for this reference base
+        self.fusion_seq_dict[reference_base][5] = anchor
+            #print("stored after: {}".format(self.fusion_seq_dict[reference_base]))
+
+    def count_junc_span(self, read, breakpoint_pos, junction_count, spanning_counts, anchor):
+        """Identify whether a read belongs to a junction or spanning pair"""
+        # read overlaps the fusion breakpoint
+        if breakpoint_pos >= self.bp_distance_threshold and read.get_overlap(breakpoint_pos - self.bp_distance_threshold, breakpoint_pos + self.bp_distance_threshold) == 2 * self.bp_distance_threshold:
+            junction_count += 1
+            anchor = max(anchor, min(read.reference_end - breakpoint_pos, breakpoint_pos - read.reference_start))
+        # read maps left of the fusion breakpoint
+        if read.reference_end < (breakpoint_pos + self.bp_distance_threshold):
+            spanning_counts[0] += 1
+        # read maps right of the fusion breakpoint
+        if read.reference_start > (breakpoint_pos - self.bp_distance_threshold):
+            spanning_counts[1] += 1
+        return junction_count, spanning_counts, anchor
+
+    # counter_start should be 1 for ft, 6 for wt1 and 11 for wt2 (see comment in header_to_dict for details)
+    def update_counts(self, reference_base, junctions, spannings, counter_start):
+        """Updates the read counter in the fusion_seq_dict"""
+        if spannings[0] > 0:
+            self.fusion_seq_dict[reference_base][1] += 1
+        if spannings[1] > 0:
+            self.fusion_seq_dict[reference_base][2] += 1
+        if junctions > 0:
+            self.fusion_seq_dict[reference_base][3] += 1
+        elif spannings[0] > 0 and spannings[0] == spannings[1]:
+            self.fusion_seq_dict[reference_base][4] += 1
+
+    def normalize_counts_cpm(self, count):
+        """Returns the CPM of a read count based on the number of original input reads"""
+        if self.input_read_count == 0:
+            return count
+        return count / self.input_read_count * 1000000
+
+    def run(self):
+        """Walk linewise through a s/bam file and send reads mapping to the same fusion/wt context to counting"""
+        #self.logger.info("Starting fusion read filtering")
+        count_lines = 0
+        count_processed_refs = 0
+        # the read buffer is a dict of lists. The dict has query names as keys and a list of corresponding reads as values.
+        read_buffer = {}
+        last_reference = ""
+
+        for read in self.in_bam.fetch():
+            count_lines += 1
+            if not last_reference in read.reference_name and count_lines > 1:
+                self.quantify_read_groups(read_buffer, last_reference)
+                count_processed_refs += 1
+                if count_processed_refs % (len(self.fusion_seq_dict) / 10) == 0:
+                    print("At least {}% completed...".format(count_processed_refs / (len(self.fusion_seq_dict) / 100)))
+                read_buffer.clear()
+            if not read.query_name in read_buffer:
+                read_buffer[read.query_name] = []
+            read_buffer[read.query_name].append(read)
+            # reference name conventions may change in the near future, but the tail must be
+            # "*_breakpointPosition_ft" for the fusion transcript and "*_wt{1,2}" for the wt background
+#            if read.reference_name.endswith("ft"):
+            last_reference = read.reference_name
+#            else:
+#                last_reference = "_".join(read.reference_name.split("_")[:-1])
+
+        # process the last read_buffer
+        self.quantify_read_groups(read_buffer, last_reference)
+        print("All done! Reads did not map to ~{}% of the reference input".format(100 - (count_processed_refs / (len(self.fusion_seq_dict) / 100))))
+        # close reading/writing stream
+        self.in_bam.close()
+        # write data to file. Change out_file_sep to ";" to create a csv file
+
+        # get input read count
+        with open(os.path.join(os.path.dirname(self.output), "star", "Log.final.out"), "r") as rfile:
+            for i, line in enumerate(rfile):
+                elements = line.rstrip().split()
+                if i == 5:
+                    self.input_read_count = int(elements[-1])
+
+        out_file_sep = ";"
+        header_string = out_file_sep.join(
+            ["fgid",
+             "bp", "a", "b", "junc", "span", "anch"])
+        # write counts and normalized counts
+        # for normalization, breakpoint and anchor must not be converted: list positions 0, 5, 10, 15
+        no_norm = {0, 5, 6, 11, 12, 17}
+        with open("{}.counts".format(self.output), "w") as out_counts, open(self.output, "w") as out_norm:
+            out_counts.write("{}\n".format(header_string))
+            out_norm.write("{}\n".format(header_string))
+            for key in self.fusion_seq_dict:
+                out_counts.write("{}{}".format(key, out_file_sep))
+                out_norm.write("{}{}".format(key, out_file_sep))
+                # write counts directly
+                out_counts.write("{}\n".format(out_file_sep.join(map(str, self.fusion_seq_dict[key]))))
+                # normalize and write normalized counts
+                self.fusion_seq_dict[key] = [read_count if i in no_norm else self.normalize_counts_cpm(read_count) for i, read_count in enumerate(self.fusion_seq_dict[key])]
+                #self.fusion_seq_dict[key] = map(self.normalize_counts_cpm, self.fusion_seq_dict[key])
+                out_norm.write("{}\n".format(out_file_sep.join(map(str, self.fusion_seq_dict[key]))))
+        # write normalized counts
+#        with
+#            out_file2.write("{}\n".format(header_string))
+#            for key in self.fusion_seq_dict:
+                # write only those putative fusions, where not all counts are 0
+                #if sum(self.fusion_seq_dict[key][1:]) > 0:
+                #self.fusion_seq_dict[key].insert(0, key.rsplit("_", 2)[0])
+#                self.fusion_seq_dict[key] = map(self.normalize_counts_cpm, self.fusion_seq_dict[key])
+#                self.fusion_seq_dict[key].insert(0, key)
+#                out_file.write("{}\n".format(out_file_sep.join(map(str, self.fusion_seq_dict[key]))))
+
+#        out_file_sep = ";"
+#        with open(self.output, "w") as out_file:
+#            out_file.write(out_file_sep.join(["ftid_plus", "Type", "Reads_Gene_A", "Reads_Gene_B", "Junction_Reads", "Spanning_Pairs", "Longest_Anchor"]) + "\n")
+#            for key in self.fusion_seq_dict:
+#                # write only those putative fusions, where not all counts are 0
+#                #if sum(self.fusion_seq_dict[key][1:]) > 0:
+#                #ABCA7_19:1059141:+_ENST00000532194_DNHD1_11:6546666:+_ENST00000533649_9dd39c73a904776e_100_ft
+#                ftid_p = key.rsplit("_", 2)[0]
+#                (_, junc_ft, span_ft, anch_ft, junc_wt1, span_wt1, anch_wt1, junc_wt2, span_wt2, anch_wt2) = self.fusion_seq_dict[key]
+#                out_file.write(out_file_sep.join(map(str, [ftid_p, "ft", "NA", "NA", junc_ft, span_ft, anch_ft])) + "\n")
+#                out_file.write(out_file_sep.join(map(str, [ftid_p, "wt1", "NA", "NA", junc_wt1, span_wt1, anch_wt1])) + "\n")
+#                out_file.write(out_file_sep.join(map(str, [ftid_p, "wt2", "NA", "NA", junc_wt2, span_wt2, anch_wt2])) + "\n")
+
+def main():
+    """Parse command line arguments and start script"""
+    parser = ArgumentParser(description="Generate mapping stats for fusion detection")
+    parser.add_argument('-i', '--input', dest='input', help='Specify input BAM file', required=True)
+    parser.add_argument('-b', '--bed', dest='bed', help='Specify input BED file', required=True)
+    parser.add_argument('-o', '--output', dest='output', help='Specify output file', required=True)
+    parser.add_argument('-d', '--bp_distance', dest='bp_distance', help='Threshold of bases around the breakpoint for junction/spanning counting', default="3")
+    args = parser.parse_args()
+
+    requant = Requantification(args.input, args.bed, args.output, args.bp_distance)
+    requant.run()
+
+if __name__ == '__main__':
+    main()
