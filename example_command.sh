6338a7009b8cb75006a511ed193963993116315c
ibn-salem <jonas.ibn-salem@tron-mainz.de>
2019-07-05 17:54:40 +0200

    update example_comand.sh with tissue sample

--- example_command.sh
+++ example_command.sh
@@ -7,8 +7,18 @@ python /home/sorn/development/ngs_pipelines/neo_orf_analysis/get_neo_orfs.py \
   -p CentOS
 
 
+# Innotop stomach samples
+# IT_N_104
+# IT_N_105
+# and others
+
+# Innotop Lung samples:
+# IT_N_222
+# IT_N_223
+# and others
+
+
 python easy_quant.py \
-  -i /miseq/190313_M04039_0060_000000000-CB57P/Data/Intensities/BaseCalls/Library-1_S1_L001_R1_001.fastq.gz /miseq/190313_M04039_0060_000000000-CB57P/Data/Intensities/BaseCalls/Library-1_S1_L001_R2_001.fastq.gz \
-  -f example_data/CLDN18_Context_seqs.csv \
-  -o example_data
-  
+  -i /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R1_001.fastq.gz /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R2_001.fastq.gz \
+  -f example_data/CLDN18_Context_seqs_reformated.csv \
+  -o example_out
