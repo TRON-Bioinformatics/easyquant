528be96540ac947be6c49f845b892a76303cca8a
ibn-salem <jonas.ibn-salem@tron-mainz.de>
2019-07-08 15:19:44 +0200

    update example_command.sh (need further simplifications)

--- example_command.sh
+++ example_command.sh
@@ -17,8 +17,57 @@ python /home/sorn/development/ngs_pipelines/neo_orf_analysis/get_neo_orfs.py \
 # IT_N_223
 # and others
 
+mkdir -p /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq
 
+zcat /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R1_001.fastq.gz \
+ | head -n 40000 \
+ | gzip \
+ > /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R1_001.h10k.fastq.gz
+
+zcat /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R2_001.fastq.gz \
+ | head -n 40000 \
+ | gzip \
+ > /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R2_001.h10k.fastq.gz
+
+
+# build toy example fastqs fom mapping reads
+module load bioinf/samtools/1.9
+module load bioinf/bedtools/2.27.1
+module load python/2.7.15
+
+samtools sort -n /projects/CM29_RNA_Seq/easyquant/easyquant/Aligned.sortedByCoord.out.bam \
+  -o /projects/CM29_RNA_Seq/easyquant/easyquant/Aligned.sortedByCoord.out.qsort.bam
+
+bamToFastq -i /projects/CM29_RNA_Seq/easyquant/easyquant/Aligned.sortedByCoord.out.qsort.bam \
+  -fq /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R1_001.mapping.fastq \
+  -fq2 /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R2_001.mapping.fastq
+
+gzip /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R1_001.mapping.fastq \
+  > /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R1_001.mapping.fastq.gz
+gzip /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R2_001.mapping.fastq \
+  > /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R2_001.mapping.fastq.gz
+
+# # samll referece genome
+# python easy_quant.py \
+#   -i /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R1_001.h10k.fastq.gz \
+#      /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R2_001.h10k.fastq.gz \
+#   -f /projects/CM29_RNA_Seq/easyquant/easyquant/example_data/CLDN18_Context_seqs_reformated.csv \
+#   -o /projects/CM29_RNA_Seq/easyquant/easyquant/example_out_small
+
+# samll data example
 python easy_quant.py \
-  -i /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R1_001.fastq.gz /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R2_001.fastq.gz \
-  -f example_data/CLDN18_Context_seqs_reformated.csv \
-  -o example_out
+  -i /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R1_001.mapping.fastq.gz \
+     /scratch/info/projects/CM29_RNA_Seq/easyquant/test_fq/IT_N_222_SL1_S5_L005_R2_001.mapping.fastq.gz \
+  -f /projects/CM29_RNA_Seq/easyquant/easyquant/example_data/CLDN18_Context_seqs_reformated.csv \
+  -o /projects/CM29_RNA_Seq/easyquant/easyquant/example_out_3
+
+# 
+# # full example
+# python easy_quant.py \
+#   -i /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R1_001.fastq.gz \
+#      /scratch/info/data/CM29_RNA_Seq/innotop_all/IT_N_222_SL1_S5_L005_R2_001.fastq.gz \
+#   -f /projects/CM29_RNA_Seq/easyquant/easyquant/example_data/CLDN18_Context_seqs_reformated_new.csv \
+#   -o /projects/CM29_RNA_Seq/easyquant/easyquant/example_out_full
+  
+  
+  
