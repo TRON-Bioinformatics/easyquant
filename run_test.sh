c8a2a96f999513a41ba4de6c000412e3a1f1ddaf
ibn-salem <jonas.ibn-salem@tron-mainz.de>
2019-07-09 10:01:51 +0200

    add bash script to run test on provided example data

---
+++ run_test.sh
@@ -0,0 +1,7 @@
+
+# Run example data 
+
+python easy_quant.py \
+  -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
+  -s example_data/CLDN18_Context_seq.csv \
+  -o example_out
