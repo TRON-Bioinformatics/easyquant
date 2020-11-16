b1559cefec4b094439a37d1cdaa90423b911acb1
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2020-11-16 12:16:45 +0100

    add further exaples to run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,30 @@
 
-# Run example data 
+module load  python/2.7.15
 
+# Run example data 
 python easy_quant.py \
   -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
   -s example_data/CLDN18_Context_seq.csv \
   -o example_out
+
+python easy_quant.py \
+  -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
+  -s example_data/CLDN18_Context_seq.csv \
+  -o example_out_0.2
+
+# multiple sample input
+python easy_quant.py \
+  -i example_data/example_rna-seq_R1_001.fastq.gz \
+      example_data/example_rna-seq_R2_001.fastq.gz \
+      example_data/example_02_rna-seq_R1_001.fastq.gz \
+      example_data/example_02_rna-seq_R2_001.fastq.gz \
+  -d 3 \
+  -s example_data/CLDN18_Context_seq.csv \
+  -o example_out_mult
+
+# mismatch rate example 
+python easy_quant.py \
+  -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
+  -s example_data/CLDN18_Context_seq.csv \
+  -o example_out_mmrate
+
