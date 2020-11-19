ac3771aefb119abf6944f65e621eddc8552adf34
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2020-11-19 17:35:39 +0100

    Implement bash script for STAR mapping with changed parameters

--- star_alignment.sh
+++ star_alignment.sh
@@ -1,38 +1,41 @@
 
+star_bin="/code/STAR/2.6.1d/bin/Linux_x86_64_static/STAR"
+samtools_bin="/code/samtools/1.9/samtools"
 
 fq1=example_data/example_rna-seq_R1_001.fastq.gz
 fq2=example_data/example_rna-seq_R2_001.fastq.gz
-ncpu=2
+ncpu=12
+aln_out_dir="star_aln"
 
-mkdir bwa_ref
-mkdir bwa_aln
+mkdir star_ref
+mkdir ${aln_out_dir}
 
-cp example_out_0.2/context.fa bwa_ref/context.fa
+cp example_out_0.2/context.fa star_ref/context.fa
 
-# index
-bwa index bwa_ref/context.fa
+# Generate index
+${star_bin} --runMode genomeGenerate \
+  --runThreadN ${ncpu} \
+  --genomeSAindexNbases 4 \
+  --genomeDir star_ref/ \
+  --genomeFastaFiles star_ref/context.fa
 
-bwa aln -t ${ncpu} bwa_ref/context.fa ${fq1} > bwa_aln/`basename $fq1`.sai
-bwa aln -t ${ncpu} bwa_ref/context.fa ${fq2} > bwa_aln/`basename $fq2`.sai
-bwa sampe bwa_ref/context.fa bwa_aln/`basename $fq1`.sai bwa_aln/`basename $fq2`.sai ${fq1} ${fq2}  \
-  | samtools view -uS - \
-  | samtools sort  \
-  > bwa_aln/example_rna-seq.bam
-samtools index bwa_aln/example_rna-seq.bam
-
-
-/code/STAR/2.6.1d/bin/Linux_x86_64_static/STAR \
-  --outFileNamePrefix example_out_0.2/star/ \
+# map reads
+# for STAR manual see: https://raw.githubusercontent.com/alexdobin/STAR/master/doc/STARmanual.pdf
+${star_bin} \
+  --outFileNamePrefix ${aln_out_dir}/ \
   --limitOutSAMoneReadBytes 1000000 \
-  --genomeDir example_out_0.2/STAR_idx \
+  --genomeDir star_ref \
   --readFilesCommand 'gzip -d -c -f' \
-  --readFilesIn example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
+  --readFilesIn ${fq1} ${fq2} \
   --outSAMmode Full \
   --outFilterMultimapNmax -1 \
-  --outSAMattributes Standard \
+  --outSAMattributes NH HI AS nM NM \
   --outSAMunmapped None \
   --outFilterMismatchNoverLmax 0.02 \
-  -outFilterMismatchNoverReadLmax 0.02 \
+  --outFilterMismatchNoverReadLmax 0.02 \
   --outSAMtype BAM SortedByCoordinate \
-  --runThreadN 12
+  --runThreadN ${ncpu}
+
+# index output bam
+${samtools_bin} index ${aln_out_dir}/Aligned.sortedByCoord.out.bam
 
