f2d0ce2ec47c8fa212272eca5366b971e22aa4a6
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2020-11-20 10:44:51 +0100

    local changes in run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ module load  python/2.7.15
 python easy_quant.py \
   -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
   -s example_data/CLDN18_Context_seq.csv \
-  -o example_out
+  -o example_out_fix_insertion
 
 python easy_quant.py \
   -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
