46d9a255d2c0dd777cc4755544007663085728a2
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2021-11-15 20:43:31 +0100

    simplify test run on provided example data

--- run_test.sh
+++ run_test.sh
@@ -1,18 +1,13 @@
 
-module load anaconda/3/2019
+# module load anaconda/3/2019
 
-# Test run for requantify.py
-# /code/Anaconda/3/2019/bin/python requantify.py \
-python requantify.py \
-  -i star_aln/Aligned.sortedByCoord.out.bam \
-  -t example_data/CLDN18_Context_seq.csv \
-  -d 10 \
-  -o CLDN18_Context_seq.csv.counts_new5_star.tsv
-
+#================================================================
 # Test run for easy_qant.py on example data
+#================================================================
 python easy_quant.py \
   -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
   -s example_data/CLDN18_Context_seq.csv \
+  -d 10 \
   -o example_out_csv
 
 #================================================================
@@ -22,39 +17,3 @@ python easy_quant.py \
   -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
   -s example_data/CLDN18_Context_seq.tsv \
   -o example_out_tab
-
-# python requantify.py \
-#   -i example_out_tab/star/Aligned.sortedByCoord.out.bam \
-#   -t example_data/CLDN18_Context_seq.tsv \
-#   -d 10 \
-#   -o example_out_tab/quantification.tsv
-
-#================================================================
-
-# Run easyqant on example data
-python easy_quant.py \
-  -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
-  -s example_data/CLDN18_Context_seq.csv \
-  -o example_out_fix_insertion
-
-python easy_quant.py \
-  -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
-  -s example_data/CLDN18_Context_seq.csv \
-  -o example_out_0.2
-
-# multiple sample input
-python easy_quant.py \
-  -i example_data/example_rna-seq_R1_001.fastq.gz \
-      example_data/example_rna-seq_R2_001.fastq.gz \
-      example_data/example_02_rna-seq_R1_001.fastq.gz \
-      example_data/example_02_rna-seq_R2_001.fastq.gz \
-  -d 3 \
-  -s example_data/CLDN18_Context_seq.csv \
-  -o example_out_mult
-
-# mismatch rate example 
-python easy_quant.py \
-  -i example_data/example_rna-seq_R1_001.fastq.gz example_data/example_rna-seq_R2_001.fastq.gz \
-  -s example_data/CLDN18_Context_seq.csv \
-  -o example_out_mmrate
-
