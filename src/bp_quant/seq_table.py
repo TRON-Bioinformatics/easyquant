1946787c94d0dd096457a7e5b4644c7ca295b701
Patrick Sorn <patrick.sorn@tron-mainz.de>
2024-09-20 17:55:27 +0200

    Refactored read classification; Added additional unit tests

---
+++ src/bp_quant/seq_table.py
@@ -0,0 +1,52 @@
+
+import csv
+# import logging
+import sys
+
+csv.field_size_limit(sys.maxsize)
+
+# logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+# logger = logging.getLogger(__name__)
+
+def generate_intervals(pos_arr: list) -> list:
+    intervals = []
+    for i in range(len(pos_arr)-1):
+        interval_name = "{}_{}".format(pos_arr[i], pos_arr[i+1])
+        intervals.append((interval_name, int(pos_arr[i]), int(pos_arr[i+1])))
+    return intervals
+
+
+def read_table(seq_table_file: str):
+    #logger.info("Parsing input sequences from file (path={}).".format(seq_table_file))
+
+    with open(seq_table_file, "r") as csvfile:
+        # Auto detect dialect of input file
+        dialect = csv.Sniffer().sniff(csvfile.readline(), delimiters=";,\t")
+        csvfile.seek(0)
+        reader = csv.DictReader(csvfile, dialect=dialect)
+        for row in reader:
+            yield row
+
+
+def get_seq_to_pos_dict(seq_table_file: str) -> dict:
+    """
+    Parses the sequence table and returns a dict where each sequence name is mapped to intervals
+    """
+
+    # seq_to_pos is a dict from sequence name to the position of interest (breakpoint or junction)
+    seq_to_pos = {}
+
+    # Iterate over input file rows
+    for row in read_table(seq_table_file):
+        name = row["name"]
+
+        pos_arr = None
+        
+        if "," in row["position"]:
+            pos_arr = row["position"].split(",")
+        else:
+            pos_arr = [0, row["position"], len(row["sequence"])]
+        intervals = generate_intervals(pos_arr)
+        seq_to_pos[name] = (intervals, ",".join([str(x) for x in pos_arr]))
+    return seq_to_pos
---
+++ tests/test_alignment_info.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import unittest
+
+import pysam
+
+from bp_quant.alignment_info import get_aligner
+from bp_quant.alignment_info import get_sorting
+from bp_quant.alignment_info import is_chimeric_alignment
+from bp_quant.alignment_info import is_singleton
+
+BAM_FILE = os.path.join("example_data", "example_rna-seq.bam")
+
+class TestAlignmentInfo(unittest.TestCase):
+
+    def setUp(self):
+        # TODO: Mock alignment object
+        self.aln_obj = pysam.AlignmentFile(BAM_FILE, "rb")
+
+    def test_get_aligner(self):
+        self.assertEqual(get_aligner(self.aln_obj), "star")
+
+
+    def test_get_sorting(self):
+        self.assertEqual(get_sorting(self.aln_obj), "")
+
+    def test_is_chimeric(self):
+        # Test that chimeric read is correctly identified
+        read = pysam.AlignedSegment()
+        read.query_name = "read_28833_29006_6945"
+        read.flag = 99
+        read.reference_id = 0
+        read.reference_start = 32
+        read.next_reference_id = 3
+        self.assertTrue(is_chimeric_alignment(read))
+        
+        # Check that aligned read with unmapped mapped is identified as valid alignment
+        read = pysam.AlignedSegment()
+        read.query_name = "read_28833_29006_6945"
+        read.flag = 105
+        read.reference_id = 0
+        read.reference_start = 32
+        read.next_reference_id = -1
+        self.assertFalse(is_chimeric_alignment(read))
+
+        # Check that unaligned read with mapped mate is identified as valid alignment
+        read = pysam.AlignedSegment()
+        read.query_name = "read_28833_29006_6945"
+        read.flag = 101
+        read.reference_id = -1
+        read.reference_start = 32
+        read.next_reference_id = 3
+        self.assertFalse(is_chimeric_alignment(read))
+
+    def test_is_singleton(self):
+        
+        # Check that aligned read with unmapped mapped is identified as valid alignment
+        read = pysam.AlignedSegment()
+        read.query_name = "read_28833_29006_6945"
+        read.flag = 105
+        read.reference_id = 0
+        read.reference_start = 32
+        read.next_reference_id = -1
+        self.assertTrue(is_singleton(read))
+
+        # Check that unaligned read with mapped mate is identified as valid alignment
+        read = pysam.AlignedSegment()
+        read.query_name = "read_28833_29006_6945"
+        read.flag = 101
+        read.reference_id = -1
+        read.reference_start = 32
+        read.next_reference_id = 3
+        self.assertTrue(is_singleton(read))
